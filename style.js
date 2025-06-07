6a709c4e2b545d73f6c7b98e388c24a7ad1dcc75
mayanksinha <mayanksinha2399@gmail.com>
2025-06-07 16:48:03 +0530

    change in rcw module

--- style.js
+++ style.js
@@ -1,59 +1,118 @@
- function showDestination(data) {
-    var carousels=document.getElementById('carousels')
-    carousels.style.display='none';
 
-    const destination = document.getElementById('destination');
-
-
-    const destinationImage = document.createElement('div');
-    destinationImage.className = 'destination-images';
+const resources = document.getElementById('Resources-div');
+const sotd = document.getElementById('sotd-div');
+const practice = document.getElementById('practice-div');
+const h_view = document.getElementById('h-carousel-div');
 
 
-    const leftDestinationImage =document.createElement('div');
-    leftDestinationImage.className ='left-destination-image';
-    const lefImg = document.createElement('img');
-    lefImg.src = data.gallery_media[0].media_urls.original;
-    leftDestinationImage.appendChild(lefImg)
+const resourcesButton = document.getElementById('resources');
+const sotdButton = document.getElementById('sotd');
+const practiceButton = document.getElementById('practice');
+
+history.replaceState({ page: 'home' }, '', '');
 
 
-    const rightDestinationImage =document.createElement('div');
-    rightDestinationImage.className ='right-destination-image';
-    const rightImg1 = document.createElement('img');
-    rightImg1.src = data.gallery_media[1].media_urls.original;
-    rightDestinationImage.appendChild(rightImg1)
+practiceButton.onclick = function () {
+    resources.style.display = 'none';
+    sotd.style.display='none'
+    h_view.style.display='none'
+    practice.style.display = 'block';
+    history.pushState({ page: 'practice' }, '', '#practice');
+};
 
-    const rightImg2 = document.createElement('img');
-    rightImg2.src = data.gallery_media[2].media_urls.original;
-    rightDestinationImage.appendChild(rightImg2)
+sotdButton.onclick = function () {
+    resources.style.display = 'none';
+    practice.style.display='none'
+    h_view.style.display='none'
+    sotd.style.display = 'block';
+    history.pushState({ page: 'sotd' }, '', '#sotd');
+};
+
+resourcesButton.onclick = function () {
+    practice.style.display = 'none';
+    sotd.style.display='none'
+    resources.style.display = 'block';
+    h_view.style.display='none'
+    history.pushState({ page: 'resources' }, '', '#resources');
+};
 
 
-    destinationImage.appendChild(leftDestinationImage);
-    destinationImage.appendChild(rightDestinationImage);
+async function loadHeader(key,value){
+    var upperHeader= document.getElementById('upper-header-2');
+    const p =document.createElement('p');
+    p.className ='header'+key;
+    p.innerText= key;
 
+     p.addEventListener('click', () => {
+        console.log(value) 
+         practice.style.display = 'none';
+        sotd.style.display='none'
+        resources.style.display='none'
+        h_view.style.display = 'block';
+        history.pushState({ page: 'h_view' }, '', '#'+key);       
+        test(key,value);
 
-    const p1 =document.createElement('p');
-    p1.className ='destination-title';
-    p1.innerText= data.name;
+    });
+    upperHeader.appendChild(p);
 
-    const p2 =document.createElement('p');
-    p2.className ='destination-title';
-    p2.innerText=data.currency +" "+data.starting_price;
-
-    destination.appendChild(destinationImage);
-    destination.appendChild(p1);
-    destination.appendChild(p2);
  }
 
+ async function test(key,value) {
+  const h_carousel_div=document.getElementById("h-carousel-div");
+   const section = document.createElement('div');
+          section.className = 'h-carousel'
+
+          const title = document.createElement('h2');
+          title.className = 'carousel-title';
+          title.textContent =key;
+
+          const carousel = document.createElement('div');
+          carousel.className = 'h-carousel';
+        value.forEach(item => {
+            const card = document.createElement('div');
+            card.className = 'carousel-item';
+
+            const img = document.createElement('img');
+            img.src = "images/rubic_cube_world.jpg";
+
+            const heading = document.createElement('h3');
+            heading.textContent = item.name;
+
+             const h4 = document.createElement('h4');
+            h4.textContent = item.currency +" "+item.starting_price;
+
+            const link = document.createElement('a');
+            link.href = item.link;
+            link.textContent = 'Learn more';
+            link.target = '_blank';
+
+            card.appendChild(img);
+            card.appendChild(heading);
+            card.appendChild(h4);
+            card.appendChild(link);
+
+            card.addEventListener('click', () => {
+              // showDestination(item)
+            });
+            carousel.appendChild(card);
+          });
+
+          section.appendChild(title);
+          section.appendChild(carousel);
+          h_carousel_div.appendChild(section);
+ }
 
  fetch('data.json')
       .then(response => response.json())
       .then(data => dataPopulate(data));
     function dataPopulate(data) {
+
         const container = document.getElementById('carousels');
 
         Object.keys(data).forEach(key => {
+          loadHeader(key,data[key]);
           const section = document.createElement('div');
-          section.className = 'carousel-container';
+          section.className = 'carousel'
 
           const title = document.createElement('h2');
           title.className = 'carousel-title';
@@ -67,7 +126,7 @@
             card.className = 'carousel-item';
 
             const img = document.createElement('img');
-            img.src = item.gallery_media[0].media_urls.original;
+            img.src = "images/rubic_cube_world.jpg";
 
             const heading = document.createElement('h3');
             heading.textContent = item.name;
@@ -77,7 +136,7 @@
 
             const link = document.createElement('a');
             link.href = item.link;
-            link.textContent = 'Request Callback';
+            link.textContent = 'Learn More';
             link.target = '_blank';
 
             card.appendChild(img);
@@ -86,9 +145,7 @@
             card.appendChild(link);
 
             card.addEventListener('click', () => {
-              showDestination(item)
-              // document.querySelectorAll('.sidebar-child').forEach(el => el.style.display = 'none');
-              //  submenu.style.display = 'block';
+              // showDestination(item)
             });
             carousel.appendChild(card);
           });
